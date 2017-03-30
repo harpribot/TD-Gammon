ceda3ff04af7a0253dc21827c8a85fe96c634cce
Garrett <garetWK@users.noreply.github.com>
2017-03-29 21:31:51 -0500

    Modified printMoveEvaluations
    original evaluations are the opponents probability of winning that you
    try to minimize. For printing this is flip to show your probability of
    winning.

--- printMoveEvaluations.m
+++ printMoveEvaluations.m
@@ -1,9 +1,11 @@
 % Copyright @2017 MIT License - Author - Garrett Kaiser
 % See the License document for further information
 function printMoveEvaluations(table)
-
+    % original evaluations are the opponents probability of winning that
+    % you try to minimize. For printing this is flip to show your
+    % probability of winning.
+    table(:,1) = 1-table(:,1);
     fprintf('\tEval\tMove\n');
     fprintf('\t%.4f\t[%3d ->%3d    %3d ->%3d    %3d ->%3d    %3d ->%3d ]\n', table.');
     fprintf('\n');
-
 end
